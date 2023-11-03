a57b2911580097701536bc61f54d6c3f105557ff
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2023-11-03 13:18:43 +0700

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as AddStudent } from "./AddStudent";
+export { default as EditStudent } from "./EditStudent";
+export { default as StudentDetails } from "./StudentDetails";
+export { default as StudentList } from "./StudentList";
